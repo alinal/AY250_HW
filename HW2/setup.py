1af0b5a70dd554498ac01fe15485ee8eac0d477f
Alina Liberman <alinal@berkeley.edu>
2013-09-16 16:53:02 -0700

    Added setup file

---
+++ HW2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+
+setup(name='CalCalc',
+      version='1.0',
+      description='Evaluates strings with eval or Wolfram|Alpha',
+      author='Alina Liberman',
+      author_email='alinal@berkeley.edu',
+      url='https://github.com/alinal/AY250.git',
+      packages=['CalCalc', 'CalCalc.calculate'],
+     )
