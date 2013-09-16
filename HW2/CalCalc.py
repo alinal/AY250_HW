5ace5253d4506976d073f875c50f741e430d61e5
Alina Liberman <alinal@berkeley.edu>
2013-09-15 22:06:02 -0700

    Add init file and update CalCalc.py

--- HW2/CalCalc.py
+++ HW2/CalCalc.py
@@ -1,3 +1,14 @@
-from datagen import generateData
-from plotting import histOutline
 import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', action='store_true', default=False, dest='Unfilled_Histogram',
+                    help='True to use an unfilled histogram')
+
+parser.add_argument('-n', action='store',default=10, type=int,  dest='dataN',
+                    help='Store a constant value')
+
+parser.add_argument('-T', action='store', default='No Title', dest='title',
+                    help='Store a simple value')
+
+
+results = parser.parse_args()
