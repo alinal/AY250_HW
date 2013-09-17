8d0e1a0f44c94c469a352638f502b79ac42cb772
Alina Liberman <alinal@berkeley.edu>
2013-09-16 17:06:45 -0700

    Updated folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+
+setup(name='CalCalc',
+      version='1.0',
+      description='Evaluates strings with eval or Wolfram|Alpha',
+      author='Alina Liberman',
+      author_email='alinal@berkeley.edu',
+      url='https://github.com/alinal/AY250/HW2.git',
+      packages=['CalCalc'])
